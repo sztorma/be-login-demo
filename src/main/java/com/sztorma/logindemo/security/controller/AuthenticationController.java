2e5be2548c965d5aedbf78c89ecd59ac95b193f9
sztorma <sztorma@outlook.com>
2022-04-05 18:03:35 +0200

    auth facade

--- src/main/java/com/sztorma/logindemo/security/controller/AuthenticationController.java
+++ src/main/java/com/sztorma/logindemo/security/controller/AuthenticationController.java
@@ -1,18 +1,12 @@
 package com.sztorma.logindemo.security.controller;
 
-import com.sztorma.logindemo.security.JwtTokenUtil;
-import com.sztorma.logindemo.security.JwtUserDetailsService;
+import com.sztorma.logindemo.security.facade.AuthenticationFacade;
 import com.sztorma.logindemo.security.model.JwtRequest;
 import com.sztorma.logindemo.security.model.JwtResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,34 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil;
-
-    @Autowired
-    private JwtUserDetailsService userDetailsService;
+    private AuthenticationFacade authenticationFacade;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public ResponseEntity<JwtResponse> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
             throws Exception {
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-        final UserDetails userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.getUsername());
-
-        final String token = jwtTokenUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(new JwtResponse(token));
+        JwtResponse response = authenticationFacade.createAuthenticationResponse(authenticationRequest);
+        return ResponseEntity.ok(response);
     }
 
-    private void authenticate(String username, String password) throws Exception {
-        try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException e) {
-            throw new Exception("USER_DISABLED", e);
-        } catch (BadCredentialsException e) {
-            System.out.println("bad credential");
-            throw new Exception("INVALID_CREDENTIALS", e);
-        }
-    }
 }
---
+++ src/main/java/com/sztorma/logindemo/security/facade/AuthenticationFacade.java
@@ -0,0 +1,48 @@
+package com.sztorma.logindemo.security.facade;
+
+import com.sztorma.logindemo.security.JwtTokenUtil;
+import com.sztorma.logindemo.security.JwtUserDetailsService;
+import com.sztorma.logindemo.security.model.JwtRequest;
+import com.sztorma.logindemo.security.model.JwtResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class AuthenticationFacade {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @Transactional(readOnly = true)
+    public JwtResponse createAuthenticationResponse(JwtRequest authenticationRequest) throws Exception {
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return new JwtResponse(token);
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            System.out.println("bad credential");
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
