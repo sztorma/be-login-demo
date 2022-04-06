0862130c7229db687150a5bebcd404b96bd7b45a
sztorma <sztorma@outlook.com>
2022-04-06 19:19:53 +0200

    authenticated content

---
+++ src/main/java/com/sztorma/logindemo/content/controller/ContentController.java
@@ -0,0 +1,21 @@
+package com.sztorma.logindemo.content.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/content", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class ContentController {
+
+    @GetMapping(value = "/admin")
+    @ResponseBody
+    public ResponseEntity<String> adminContent() {
+        return new ResponseEntity<>(new String("{\"response\": \"response for admin\"}"), HttpStatus.OK);
+    }
+}
