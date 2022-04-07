4c79af0cec9581c6a37646d3f60162b0dfefcfb5
sztorma <sztorma@outlook.com>
2022-04-07 22:06:18 +0200

    fix duplicate transaction

--- src/main/java/com/sztorma/logindemo/security/facade/AuthenticationFacade.java
+++ src/main/java/com/sztorma/logindemo/security/facade/AuthenticationFacade.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class AuthenticationFacade {
@@ -22,7 +21,6 @@ public class AuthenticationFacade {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-    @Transactional(readOnly = true)
     public JwtResponse createAuthenticationResponse(JwtRequest authenticationRequest) throws Exception {
         final Authentication auth = authenticate(authenticationRequest.getUsername(),
                 authenticationRequest.getPassword());
