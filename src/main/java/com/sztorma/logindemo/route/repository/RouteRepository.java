49b186d515408f114292dca3da43c6bb6a184044
sztorma <sztorma@outlook.com>
2022-04-20 15:01:21 +0200

    sorting routes

--- src/main/java/com/sztorma/logindemo/route/repository/RouteRepository.java
+++ src/main/java/com/sztorma/logindemo/route/repository/RouteRepository.java
@@ -1,5 +1,6 @@
 package com.sztorma.logindemo.route.repository;
 
+import java.util.List;
 import java.util.Set;
 
 import com.sztorma.logindemo.route.entity.Route;
@@ -11,7 +12,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RouteRepository extends CrudRepository<Route, Long> {
 
-    Set<Route> findByRoleInAndComponent(Set<Role> roles, String component);
+    List<Route> findByRoleInAndComponentOrderByOrder(Set<Role> roles, String component);
 
-    Set<Route> findByComponent(String component);
 }
