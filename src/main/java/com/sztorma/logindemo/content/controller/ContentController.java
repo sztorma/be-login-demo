af3c6c68797d756cc11fc8ad727cc25cb4713c5b
sztorma <sztorma@outlook.com>
2022-04-22 11:59:38 +0200

    cross origin for content controller

--- src/main/java/com/sztorma/logindemo/content/controller/ContentController.java
+++ src/main/java/com/sztorma/logindemo/content/controller/ContentController.java
@@ -3,6 +3,7 @@ package com.sztorma.logindemo.content.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/api/content", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class ContentController {
 
